f34276efae77cdf1f697135d9750bed4f5b1d4e7
luis kihara <luiskihara@gmail.com>
2019-10-04 08:58:48 -0300

    update printers script

--- printers_counter.sh
+++ printers_counter.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -   
 #title          :printers_counter.sh
-#description    :this script it was created to collect the page counter of the printers via snmp
+#description    :this script it was created to collect the page counter of the BROTHER and HP printers via snmp
 #author         :Luis Mauricio Kihara
 #date           :20191002
-#version        :1.0.0      
+#version        :1.1.0      
 #usage          :./printers_counter.sh
 #notes          :       
 #bash_version   :5.0.7(1)-release
 #============================================================================
 
 # Init the variables
-FILE='path_of_file'
+FILE='path'
 PRINTER=$(awk '{print $2}' $FILE)
-GROUP='snmp_group'
-VERSION='version'
+GROUP='name_of_group'
+VERSION='version_of_snmp'
 MIB='1.3.6.1.2.1.43.10.2.1.4.1.1'
 
 # the script grab the printers IP of the file and create a loop, checking all the page counters and print in the screen the information
